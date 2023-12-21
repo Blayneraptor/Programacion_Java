54948ce35f1b354e6cb7149d488a3476fb1b7fb5
Blayneraptor <cousoworks@gmail.com>
2023-12-21 03:12:22 +0100

    more elegant conditionals

--- TareaCondicionales.java
+++ TareaCondicionales.java
@@ -6,28 +6,28 @@ public class TareaCondicionales {
         
         int num1,num2,num3;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Escribe un número");
+        System.out.println("Escribe un primer número");
         num1=sc.nextInt();
-        System.out.println("Escribe otro número");
+        System.out.println("Escribe un segundo número");
         num2=sc.nextInt();
-        System.out.println("Escribe otro número");
+        System.out.println("Escribe un tercer número");
         num3=sc.nextInt();
 
         
         if (num1 > num2 && num1 > num3 ) {
-        System.out.println("El número 1 es mayor que todos");
+        System.out.println("El número " + num1+ " es mayor que todos");
         } else if (num2 > num1 && num2 > num3 ){
-        System.out.println("El número 2 es mayor que todos");
+        System.out.println("El número " + num2+ " es mayor que todos");
         } else if (num3 > num1 && num3 > num2 ){
-        System.out.println("El número 3 es mayor que todos");
-        } else {}
+        System.out.println("El número " + num3+ " es mayor que todos");
+        } else{}
 
         if (num1 < num2 && num1 < num3 ) {
-        System.out.println("El número 1 es menor que todos");
+        System.out.println("El número " + num1+ " es menor que todos");
         } else if (num2 < num1 && num2 < num3 ){
-        System.out.println("El número 2 es menor que todos");
+        System.out.println("El número " + num2+ " es menor que todos");
         } else if (num3 < num1 && num3 < num2 ){
-        System.out.println("El número 3 es menor que todos");
+        System.out.println("El número " + num3+ " es menor que todos");
         }
         sc.close();
 
