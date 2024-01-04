f70f15bc45e6902f92324aa1125f94b9091f1495
Blayneraptor <cousoworks@gmail.com>
2024-01-04 01:41:34 +0100

    more exercises

---
+++ ifFactorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ifFactorial {
+
+    public static void main(String[] args) {
+        
+        int factorial=1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número para decir su factorial");
+        int num1=sc.nextInt();
+        
+        for (int i=1;i<=num1;i++){
+        factorial*=i;
+        }   
+        System.out.println(factorial);
+        sc.close();
+    }
+}
---
+++ ifTablaMulti.java
@@ -0,0 +1,20 @@
+
+public class ifTablaMulti {
+    
+    public static void main(String[] args) {
+        
+        for (int i=1;i<=10;i++){
+            System.out.println("Tabla de multiplicar del " + i + ":");
+
+            for (int a = 1; a <= 10; a++){
+            int resultado=i*a;
+            System.out.println(i + " por " + a + " es igual a : " + resultado);
+            }
+            System.out.println();
+            }
+            
+        }
+    }
+
+
+
