bab29b988a47eb8ccddae161b9cebf4efc77d0bf
Blayneraptor <cousoworks@gmail.com>
2024-02-18 02:56:21 +0100

    Array1

---
+++ Array1.java
@@ -0,0 +1,17 @@
+public class Array1 {
+
+    public static void main(String[] args) {
+       
+        int[] numeros = new int[10];
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = i + 1;
+        }
+
+       
+        System.out.println("Array en sentido inverso:");
+        for (int i = numeros.length - 1; i >= 0; i--) {
+            System.out.println(numeros[i]);
+        }
+    }
+}
