8b0fe1d3612448ec69bed23f252422dac4f6f552
Blayneraptor <cousoworks@gmail.com>
2024-02-19 06:06:29 +0100

    Package arrays

--- Arrays/Array1.java
+++ Arrays/Array1.java
@@ -1,3 +1,4 @@
+package Arrays;
 public class Array1 {
 
     public static void main(String[] args) {
--- Arrays/Array3.java
+++ Arrays/Array3.java
@@ -1,3 +1,4 @@
+package Arrays;
 public class Array3 {
     
     public static void main(String[] args) {
