c6a6f7b4c256ebac5d3730d066e4cca5d37268fa
Blayneraptor <cousoworks@gmail.com>
2024-02-19 04:41:00 +0100

    Array multidimensional

---
+++ ArrayMulti.java
@@ -0,0 +1,23 @@
+public class ArrayMulti {
+    public static void main(String[] args) {
+        
+        //Como hacer un array tridimensional
+
+        String lista[][][]= new String[3][3][3];
+
+        for (int i=0;i<3;i++){
+            
+            for (int j=0;j<3;j++){
+                
+                for (int c=0;c<3;c++){
+                    lista[i][j][c]="Fila " + i + " Columna cero " + j + " Columna uno " + c ;
+                    System.out.println(lista[i][j][c]);
+                }
+                
+            }
+            System.out.println("Fin de fila " + i + " --------------------------");
+        }
+
+
+    }
+}
