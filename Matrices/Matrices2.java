f46dfafb10ad1fd00f2a8859f5d4a22b7a8221d4
Blayneraptor <cousoworks@gmail.com>
2024-02-25 14:37:05 +0100

    Corrigiendo error yellow

--- Matrices/Matrices2.java
+++ Matrices/Matrices2.java
@@ -3,26 +3,24 @@ import java.util.Random;
 
 public class Matrices2 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         Random random = new Random();
 
+        System.out.println("Introduce el número de filas: ");
+        int filas = sc.nextInt();
         
         System.out.println("Introduce el número de columnas: ");
-        int columnas = scanner.nextInt();
+        int columnas = sc.nextInt();
 
-        
-        int[][] matriz = new int[5][columnas];
+        int[][] matriz = new int[filas][columnas];
 
-        
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
                 matriz[i][j] = random.nextInt(11); 
             }
         }
-
-        
         System.out.println("Matriz generada:");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
                 System.out.print(matriz[i][j] + " ");
             }
@@ -30,4 +28,3 @@ public class Matrices2 {
         }
     }
 }
-
