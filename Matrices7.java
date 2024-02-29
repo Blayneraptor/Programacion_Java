25717662cf6e21e52a64a13672ea7db6b07cd34b
Blayneraptor <cousoworks@gmail.com>
2024-02-29 11:11:55 +0100

    Añadiendo enunciado

---
+++ Matrices7.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Matrices7 {
+
+    /*Diseña un programa en Java que pide por pantalla una palabra de x caracteres. Se
+debe crear una matriz con el mismo número de celdas que letras tiene la palabra.
+Consideraciones:
+• Cada letra se debe almacenar en una celda.
+• Se debe comprobar si hay algún valor repetido en alguna celda. */
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe una palabra para crear una matriz con su longitud: ");
+        String palabra=sc.nextLine();
+        char matriz[][] = new char[palabra.length()][1];
+         
+        for (int i=0;i<palabra.length();i++){
+            matriz[i][0]=palabra.charAt(i);
+            System.out.print(matriz[i][0]);
+            System.out.print(" ");
+        }
+        
+
+    }
+}
