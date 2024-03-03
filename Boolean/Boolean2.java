0a45bc2e69dc50a8c58e0f11b0df43b515205d2b
Blayneraptor <cousoworks@gmail.com>
2024-03-03 19:56:27 +0100

    Nuevos ejercicios Boolean

---
+++ Boolean/Boolean2.java
@@ -0,0 +1,18 @@
+package Boolean;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Boolean2 {
+     
+        public static void main(String[] args) {
+            
+            // Escribe un programa que simule un sistema de acceso a una red social. El programa debe solicitar al usuario que ingrese su nombre de usuario y contraseña. 
+            // Luego, verifica si el nombre de usuario y la contraseña ingresados coinciden con los almacenados en el sistema.
+            //  Si coinciden, el programa debe imprimir "Acceso concedido", de lo contrario, debe imprimir "Acceso denegado".
+            Scanner sc = new Scanner (System.in);
+            ArrayList<String[]> usuarios = new ArrayList<>();
+            
+
+
+        }
+}
