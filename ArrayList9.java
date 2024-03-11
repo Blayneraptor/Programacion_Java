10bbe02af28c6d004bd4d77f79347ac4429796b4
Blayneraptor <cousoworks@gmail.com>
2024-03-12 00:40:17 +0100

    Poniendo sc.close

--- ArrayList9.java
+++ ArrayList9.java
@@ -8,20 +8,22 @@ introduce
 Autor: Jose P. Couso  "Blayneraptor"  */
 
 public class ArrayList9 {
-    
+
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        ArrayList <String> animales = new ArrayList <String> ();
-        
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> animales = new ArrayList<String>();
+
         do {
-            System.out.println("Introduce el nombre de un animal, escribe salir para terminar el programa y mostrar el contenido");
-            animales.add(sc.next());
-            
+            System.out.println(
+                    "Introduce el nombre de un animal, escribe salir para terminar el programa y mostrar el contenido");
+            animales.add(sc.nextLine());
+
         } while (!animales.contains("salir"));
-        animales.remove(animales.size()-1);
+        animales.remove(animales.size() - 1);
         System.out.println("Has salido del programa");
         System.out.println("Has introducido una cantidad de " + animales.size() + " animales.");
         System.out.println("Te muestro los animales que has introducido: " + animales);
-       
+
+        sc.close();
     }
 }
