1afe6b53be9d196b2db8b60abe99d214adf828f2
Blayneraptor <cousoworks@gmail.com>
2024-03-16 17:35:16 +0100

    Cambiando mensaje

--- Prueba2.java
+++ Prueba2.java
@@ -3,6 +3,6 @@ public class Prueba2 {
     public static void main(String[] args) {
         
 
-            System.out.println("Este mensaje es desde la rama prueba");
+            System.out.println("Este mensaje es desde la rama prueba xD");
     }
     }
