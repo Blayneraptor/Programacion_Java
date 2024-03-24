88d5b4a29600f977e27f819ae7ef380aa4c681de
Blayneraptor <cousoworks@gmail.com>
2024-03-24 01:33:23 +0100

    Creando nueva POO

---
+++ POO/PersonaPrueba/src/Persona.java
@@ -0,0 +1,16 @@
+package POO.PersonaPrueba.src;
+
+public class Persona {
+    
+    String nombre;
+    String apellido;
+
+    public void desplegarNombre(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+
+
+    }
+
+
+}
---
+++ POO/PersonaPrueba/src/PruebaPersonas.java
@@ -0,0 +1,14 @@
+package POO.PersonaPrueba.src;
+
+public class PruebaPersonas {
+    public static void main(String[] args) {
+        
+
+        Persona p1= new Persona();
+
+        p1.nombre="Armando";
+        p1.apellido="Perez";
+        
+
+    }
+}
