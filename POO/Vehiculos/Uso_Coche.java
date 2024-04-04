c97ec2010f274535ad9b7515556f209db80eba4f
Blayneraptor <cousoworks@gmail.com>
2024-04-04 23:40:43 +0200

    Editando clase

--- POO/Vehiculos/Uso_Coche.java
+++ POO/Vehiculos/Uso_Coche.java
@@ -4,9 +4,10 @@ public class Uso_Coche {
     
     public static void main(String[] args) {
         
+        Coche Renault = new Coche(); //Instanciamos la clase coche
 
+        System.out.println("Este coche tiene " + Renault.ruedas + " ruedas.");
 
-        
     }
 
 }
