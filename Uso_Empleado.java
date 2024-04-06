8082f201706a4cc4fc4b7d0532ccc7cfb0202dbc
Blayneraptor <cousoworks@gmail.com>
2024-04-06 15:07:55 +0200

    Añadiendo main

--- Uso_Empleado.java
+++ Uso_Empleado.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+import javax.swing.JOptionPane;
+
 
 //En este ejercicio hacemos combinado el Uso_Empleado y la clase Empleado
 
@@ -7,11 +9,17 @@ public class Uso_Empleado {
     
     public static void main(String[] args) {
         
+        Empleado empleado1= new Empleado("Jose Couso", 50000, 1994, 1, 24);
+        Empleado empleado2= new Empleado("Jose Perez", 40000, 1993, 2, 20);
+        Empleado empleado3= new Empleado("Jose Maria", 80000, 1995, 5, 3);
 
+        
 
     }
 
 }
+
+//<------------------------------------------------------------------------------------------>
 
 class Empleado{
 
@@ -28,5 +36,32 @@ class Empleado{
     private String nombre;
     private double sueldo;
     private Date altaContrato;
+
+
+    //Getters
+
+    public String getNombre(){
+
+        return nombre;
+    }
+
+    public double getSueldo(){
+
+        return sueldo;
+    }
+
+    public Date getaltaContrato(){
+
+        return altaContrato;
+    }
+
+    //Setters metodos
+
+    public void setSubeSueldo(double porcentaje){
+
+        double aumento=sueldo*porcentaje/100;
+        sueldo+=aumento;
+
+    }
 
 }
