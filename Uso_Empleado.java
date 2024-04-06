e6f4f29c2c6d1a42e688ef3ac6d3e0a67169d931
Blayneraptor <cousoworks@gmail.com>
2024-04-06 14:58:15 +0200

    Añadiendo GregorianCalendar

--- Uso_Empleado.java
+++ Uso_Empleado.java
@@ -1,5 +1,8 @@
 import java.util.*;
 
+
+//En este ejercicio hacemos combinado el Uso_Empleado y la clase Empleado
+
 public class Uso_Empleado {
     
     public static void main(String[] args) {
@@ -15,7 +18,10 @@ class Empleado{
     //Creamos constructor
     public Empleado (String nombre, double sueldo, int anio, int mes, int dia){
 
-
+        this.nombre=nombre;
+        this.sueldo=sueldo;
+        GregorianCalendar calendario= new GregorianCalendar(anio,mes-1,dia);
+        altaContrato=calendario.getTime();
 
     }
 
