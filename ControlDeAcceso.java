eb2f1c18bdc5ac37f99fd9762a7c0766d24cc664
Blayneraptor <cousoworks@gmail.com>
2024-04-13 02:43:41 +0200

    Añadiendo Control de accesos POO

---
+++ ControlDeAcceso.java
@@ -0,0 +1,17 @@
+public class ControlDeAcceso {
+    
+      //Atribuos
+
+      private int sumar;
+      private int restar;
+      private int multiplicar;
+      private double dividir;
+      private String salir;
+  
+      //Getters
+  
+      public int getSumar(){
+          
+      }
+  
+}
