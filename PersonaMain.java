554d76759059b21c6525bdd569f9ee0c98eb7a64
Blayneraptor <cousoworks@gmail.com>
2024-04-13 04:56:07 +0200

    Empaquetando persona2

--- PersonaMain.java
+++
@@ -1,9 +0,0 @@
-public class PersonaMain {
-    public static void main(String[] args) {
-        
-        MetodosPersona persona = new MetodosPersona();
-
-        persona.cargarDatos();
-
-    }
-}
