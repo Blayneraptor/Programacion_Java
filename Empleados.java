4f91922369f8bb0e224b89ec580e7ab474d7614a
Blayneraptor <cousoworks@gmail.com>
2024-04-19 11:57:29 +0200

    Añadiendo atributos,cons,getters,setters

---
+++ Empleados.java
@@ -0,0 +1,30 @@
+public class Empleados {
+    
+    //Atributos
+
+    private String nombre;
+
+
+    //Constructor
+
+    public Empleados(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //Getters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    //Setters
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+}
+
+
+   
