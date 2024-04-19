70bbe812ba748b21ffea8e61c9866cb47056442b
Blayneraptor <cousoworks@gmail.com>
2024-04-19 12:28:54 +0200

    Añadiendo MainEmpleados

---
+++ MainEmpleados.java
@@ -0,0 +1,18 @@
+public class MainEmpleados {
+    public static void main(String[] args) {
+        
+        //Creamos instancias de las clases
+
+        Empleados empleado = new Empleados("Juan");
+        Oficial oficial = new Oficial("Ana");
+        Operario operario = new Operario("Carlos");
+        Directivo directivo = new Directivo("Carlos");
+        
+        // Imprimir los detalles usando el método toString() de cada clase
+        
+        System.out.println(empleado);
+        System.out.println(oficial);
+        System.out.println(operario);
+        System.out.println(directivo);
+    }
+}
