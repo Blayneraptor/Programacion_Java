3fe0fe39a04826872833417e414f5637102e8634
Blayneraptor <cousoworks@gmail.com>
2024-04-19 12:30:19 +0200

    Añadiendo técnico

--- MainEmpleados.java
+++ MainEmpleados.java
@@ -7,12 +7,14 @@ public class MainEmpleados {
         Oficial oficial = new Oficial("Ana");
         Operario operario = new Operario("Carlos");
         Directivo directivo = new Directivo("Carlos");
+        Tecnico tecnico = new Tecnico("Carlos");
         
         // Imprimir los detalles usando el método toString() de cada clase
-        
+
         System.out.println(empleado);
         System.out.println(oficial);
         System.out.println(operario);
         System.out.println(directivo);
+        System.out.println(tecnico);
     }
 }
