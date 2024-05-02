c82a9acf9456cc349eeb51b9cf0e59336706a282
Blayneraptor <cousoworks@gmail.com>
2024-05-02 12:40:47 +0200

    Añadiendo atributos y constructor

--- Clases_Abstractas/Factura/Factura.java
+++ Clases_Abstractas/Factura/Factura.java
@@ -2,4 +2,21 @@ package Clases_Abstractas.Factura;
 
 public abstract class Factura {
     
+    //Atributos
+    private int nfactura;
+    private int importe;
+    private int iva;
+    private String detalle;
+    private int totalFinal;
+   
+    //Constructor
+    public Factura(int nfactura, int importe, int iva, String detalle, int totalFinal) {
+        this.nfactura = nfactura;
+        this.importe = importe;
+        this.iva = iva;
+        this.detalle = detalle;
+        this.totalFinal = totalFinal;
+    }
+
+     
 }
