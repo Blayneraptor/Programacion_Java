3b3101f4d8b1c33bd1be70e44aa8fbec5d30777c
Blayneraptor <cousoworks@gmail.com>
2024-05-07 04:43:10 +0200

    Añadiendo herencia persona

---
+++ Persona.java
@@ -0,0 +1,16 @@
+public class Persona {
+    
+    //Atributos
+
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    //Constructor
+
+    public Persona(String nombre, String apellidos, int edad){
+        this.nombre=nombre;
+        this.apellidos=apellidos;
+        this.edad=edad;
+    }
+}
