d534649e930cebcdcded5ffc04f1b45379c96c92
Blayneraptor <cousoworks@gmail.com>
2024-05-07 04:49:40 +0200

    Añadiendo setters y getters

--- Persona.java
+++ Persona.java
@@ -1,16 +1,42 @@
 public class Persona {
-    
-    //Atributos
+
+    // Atributos
 
     private String nombre;
     private String apellidos;
     private int edad;
 
-    //Constructor
+    // Constructor
 
-    public Persona(String nombre, String apellidos, int edad){
-        this.nombre=nombre;
-        this.apellidos=apellidos;
-        this.edad=edad;
+    public Persona(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    // Getters
+    public String getNombre() {
+        return "Nombre:" + nombre;
+    }
+
+    public String getApellidos() {
+        return "Apellidos:" + apellidos;
+    }
+
+    public String getEdad() {
+        return "Edad:" + edad;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
 }
